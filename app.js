a87553814da813ecf72f3cf5aa6524d7b1bdeb6b
john <johntalbot1215@gmail.com>
2016-07-10 16:12:16 -0400

    Anantomy of express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var hbs = require('express-hbs');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -15,8 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine( 'html', hbs.express4({
     partialsDir: __dirname + '/views/partials',
-    defualtLayout: __dirname + '/views/layout.html'
-});
+    defaultLayout: __dirname + '/views/layout.html'
+}) );
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+app.set('env', 'development' );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,8 +58,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
+  res.render('ERRRRRROOOOORRRR', {
+    message: err.message + "bigbig",
     error: {}
   });
 });
